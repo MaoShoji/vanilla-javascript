4ca8dd75d1803a72142fc4e15b6773cb6cb3f37a
Mao Shoji <shoji.mao.2161@members.co.jp>
2023-04-28 14:48:06 +0900

    20230430

---
+++ index.js
@@ -0,0 +1,12 @@
+const age = prompt("おいくつですか");
+
+//console.log(age);
+//console.log(parseInt(age));
+
+//if(typeof age === "number"){
+  //console.log("数字です")}
+//if(typeof age === NaN){
+  //console.log("数字じゃない")}
+
+
+  
