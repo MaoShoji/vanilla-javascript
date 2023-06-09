d22f405fd84ae4a0adbbe2491eebecd5d60a4449
Mao Shoji <shoji.mao.2161@members.co.jp>
2023-06-09 14:40:10 +0900

    0607

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,13 @@
-//let age = prompt("おいくつですか");
-//console.log(age);
-//console.log(parseInt(age));
-
-//let age = prompt("おいくつですか");
-//age = parseInt(age);利用して文字列を数字に変換
-//if (isNaN(age)) {
-//  console.log("数字じゃない");
-//} else if (age >= 30) {
-//  console.log("30より大きい");
-//} else {
-//console.log("30よりちっちゃい");
-//}
-
-//let classh1_title = document.querySelector(".classh1_title");　CSSレセクタを使用して
-//classh1_title.innerText("AAAAA");
-
-const classA = document.querySelector(".classA");
-
-// 2. constとfunctoinが何が違うの
+const h1 = document.querySelector("h1");
+const button = document.querySelector("button");
 
 function handleClick() {
-  console.log("クリックされました！！");
+  let currentH1Color = h1.style.color;
+  if (currentH1Color == "orange") {
+    h1.style.color = "teal";
+  } else {
+    h1.style.color = "orange";
+  }
 }
 
-// classA.addEventListener("click", handleClick);
-
-function testClick(type, handleClick) {
-  console.log(`Type : ${type}が実行されました。`);
-  handleClick();
-}
-
-testClick("click", handleClick);
-
-// 定義
-// classA
-// handleClick
-// testClick
-
-// 実行結果
-// Type : "click"が実行されました。
-// クリックされました！！
-
-// 実行中
-// testClick("click", handleClick);
-
-function showMessage() {
-  alert("Hello world");
-}
+button.addEventListener("click", handleClick);
